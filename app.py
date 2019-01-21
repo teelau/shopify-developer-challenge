2b55151e3d8038126b958e6030d8b548d9a0b4a0
Tommy Lau <tommylau94@gmail.com>
2019-01-20 23:05:12 -0800

    create shopping cart & return total price

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import sqlalchemy
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, Numeric
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, Numeric, ForeignKey
 import logging
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -20,23 +20,33 @@ Base = declarative_base()
 
 class Product(Base):
     __tablename__ = 'products'
-    id=Column(Integer, primary_key=True)
+    product_id=Column(Integer, primary_key=True)
     title=Column(String(32))
     price=Column(Numeric)
     inventory_count=Column(Integer)
 
     def serialize(self):
         return {
-            'id': self.id,
+            'product_id': self.product_id,
             'title': self.title,
             'price': self.price,
             'inventory_count': self.inventory_count
         }
 
+class ShoppingCart(Base):
+    __tablename__ = 'shopping_carts'
+    shopping_cart_id=Column(Integer, primary_key=True)
+
+class ShoppingCartProducts(Base):
+    __tablename__ = 'shopping_cart_products'
+    id=Column(Integer, primary_key=True)
+    shopping_cart_id=Column(Integer, ForeignKey('shopping_carts.shopping_cart_id'))
+    product_id=Column(Integer, ForeignKey('products.product_id'))
+
 def jsonify(result):
     return json.dumps([p.serialize() for p in result])
 
-# Routes
+# Route
 
 # Welcome page route
 @app.route('/')
@@ -48,9 +58,9 @@ def products():
     """
     
     """
-    product_id = request.args.get('id')
+    product_id = request.args.get('product_id')
     if product_id is not None:
-        product_result = session.query(Product).filter(Product.id == product_id).first()
+        product_result = session.query(Product).filter(Product.product_id == product_id).first()
         if product_result is None:
             return 'Product id does not exist', 404
         return json.dumps(product_result.serialize())
@@ -65,10 +75,8 @@ def products():
 
 @app.route('/purchase', methods=['PUT'])
 def purchase ():
-    product_id = request.args.get('id')
-    product_result = session.query(Product).filter(Product.id == product_id).first()
-    # logging.info(Product)
-    # return 1
+    product_id = request.args.get('product_id')
+    product_result = session.query(Product).filter(Product.product_id == product_id).first()
     if product_result is None:
         return 'Product id does not exist', 404
     if product_result.serialize()['inventory_count'] <= 0:
@@ -76,6 +84,64 @@ def purchase ():
     session.query(Product).filter(Product.id == product_id).update({'inventory_count': Product.inventory_count - 1})
     session.commit()
     return 'Purchased 1 title: {} product_id: {}'.format( product_result.serialize()['title'], product_id)
+
+@app.route('/shopping_carts', methods=['POST', 'PUT', 'GET'])
+def shopping_carts():
+
+
+    if request.method == 'POST':
+        shopping_cart = ShoppingCart()
+        session.add(shopping_cart)
+        session.commit()
+        return json.dumps({'shopping_cart_id': shopping_cart.shopping_cart_id}), 201 #shopping cart created, with id returned
+
+    elif request.method == 'PUT':
+        add_product = request.args.get('product')
+        product_result = session.query(Product).filter(Product.product_id == add_product).first()
+        if product_result is None:
+            return 'Product id does not exist', 404
+        if product_result.serialize()['inventory_count'] <= 0:
+            return 'Product is out of stock', 409
+
+        add_cart = request.args.get('cart')
+        cart_result = session.query(ShoppingCart).filter(ShoppingCart.shopping_cart_id == add_cart).first()
+        if cart_result is None:
+            return 'Cart id does not exist', 404
+        
+        cart_product_result = session.query(ShoppingCartProducts).filter(ShoppingCartProducts.product_id == add_product).filter(ShoppingCartProducts.shopping_cart_id == add_cart).first()
+        if cart_product_result is not None:
+            return 'Product already added to cart', 409
+
+        shopping_cart_products = ShoppingCartProducts(product_id=add_product, shopping_cart_id=add_cart)
+        session.add(shopping_cart_products)
+        session.commit()
+        return 'Product #{} added to cart #{}'.format(add_product, add_cart), 200
+    
+@app.route('/shopping_carts/total', methods=['GET'])
+def shopping_cart_total_price():
+    cart_id = request.args.get('cart')
+    cart_result = session.query(ShoppingCart).filter(ShoppingCart.shopping_cart_id == cart_id).first()
+
+    if cart_result is None:
+        return 'Cart id does not exist', 404
+    
+    total = session.execute(
+        ("SELECT SUM(products.price) AS 'total' FROM shopping_cart_products "
+         "LEFT JOIN products "
+         "ON products.product_id = shopping_cart_products.product_id "
+         "WHERE shopping_cart_products.shopping_cart_id = :cart_id"),
+         {"cart_id": cart_id}).first()['total']
+    
+    if total is None:
+        total = 0
+
+    return json.dumps({
+        'total': total
+    })
+
+@app.route('/shopping_carts/purchase', methods=['PUT'])
+def shopping_cart_purchase():
+    return 1        
 
 
 if __name__ == '__main__':
